7bae01f66d6dfe3ae54918b6077146055e4fbeab
atsushi130 <atsushi_main@outlook.jp>
2018-10-07 18:37:47 +0900

    define: Item model

---
+++ models/Item.ts
@@ -0,0 +1,13 @@
+
+/// Item model
+/// Copyright (c) 2018 Atsushi Miyake. All rights reserved.
+
+export class Item {
+  title: string
+  url: string
+
+  constructor(title: string, url: string) {
+    this.title = title
+    this.url  = url
+  }
+}
