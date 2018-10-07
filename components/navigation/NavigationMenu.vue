2eb902a9e287f680924f615c456fddbdc9b3de16
atsushi130 <atsushi_main@outlook.jp>
2018-10-07 18:38:04 +0900

    implement: NavigationMenu component

---
+++ components/navigation/NavigationMenu.vue
@@ -0,0 +1,41 @@
+
+ /// NavigationMenu
+ /// Copyright (c) 2018 Atsushi Miyake. All rights reserved.
+
+<template>
+  <div class="navigation__menu">
+    <ul>
+      <li v-for="item in items">
+        <a class="navigation__menu--item" href="#{ item.url }">{ item.title }</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Prop,
+  Vue
+} from "nuxt-property-decorator"
+
+@Component({})
+export default class NavigationMenu extends Vue{
+  @Prop() items
+}
+</script>
+
+<style scoped>
+.navigation__menu {
+  display: inline-block;
+
+  &--item {
+    text-decoration: none;
+  }
+
+  ul {
+    display: inline-block;
+    list-style: none;
+  }
+}
+</style>
