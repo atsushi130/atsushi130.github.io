0a0214dc0c0e017aa56089ea08d99e1a83a1710a
atsushi130 <atsushi_main@outlook.jp>
2018-10-08 22:35:38 +0900

    add: stub data

--- store/index.ts
+++ store/index.ts
@@ -15,7 +15,9 @@ export const actions = {
   async nuxtServerInit({ commit }, { app }) {
 
     const items = [
-      new Item("google", "https://google.com")
+      new Item("google", "https://google.com"),
+      new Item("Facebook", "https://www.facebook.com"),
+      new Item("Instagram", "https://www.instagram.com")
     ]
 
     commit("setItems", items)
