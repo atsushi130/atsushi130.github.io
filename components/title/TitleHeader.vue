fefea8b67624b38afd14a1d538cb0f7e19186bdf
atsushi130 <atsushi_main@outlook.jp>
2018-10-09 23:28:54 +0900

    add: TitleHeader component

---
+++ components/title/TitleHeader.vue
@@ -0,0 +1,39 @@
+
+ /// Title
+ /// Copyright (c) 2018 Atsushi Miyake. All rights reserved.
+
+<template>
+  <div class="title">
+    <p align="center">{{ title }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Prop,
+  Vue
+} from "nuxt-property-decorator"
+
+@Component
+export default class Title extends Vue {
+  name: string = "title"
+  @Prop() title: String
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  bottom: 0px;
+  left: 0px;
+  margin: auto;
+
+  p {
+    color: white;
+    font-size: 50px;
+  }
+}
+</style>
